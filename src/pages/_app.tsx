b122b6ba304d902b4e5150999c09f5f60985ff7f
Catalin Casuneanu <catalin.casuneanu@gmail.com>
2021-08-05 16:24:36 +0300

    fix: fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import { Head } from 'next/head';
 import '@/styles/global.css';
 import '@fontsource/inter';
 
@@ -10,5 +11,12 @@ if (typeof window !== `undefined`) {
 }
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
